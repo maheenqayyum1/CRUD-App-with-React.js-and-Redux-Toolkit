0a48e001c7dd98b1bc4ef7d52967a0b6d9a7c7a3
Maheen Qayyum <138582549+maheenqayyum1@users.noreply.github.com>
2024-02-06 02:10:36 +0500

    first commit

---
+++ my-app/src/components/Data.jsx
@@ -0,0 +1,17 @@
+export const userList = [
+    {
+        name: "Maheen",
+        email: "maheen@gmail.com",
+        id: 1
+    },
+    {
+        name: "Aryan",
+        email: "aryan@gmail.com",
+        id: 2
+    },
+    {
+        name: "Hoorain",
+        email: "hoorain#gmail.com",
+        id: 3
+    }
+]
---
+++ my-app/src/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+import { Provider } from 'react-redux';
+import {configureStore} from '@reduxjs/toolkit'
+import UserReducer from './components/UserReducer';
+
+const store = configureStore({
+  reducer: {
+    users: UserReducer
+  }
+})
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+
+
